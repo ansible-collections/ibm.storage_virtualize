7027562d5d2d4d97734b04dfcf974e8f643ad126
Sumit Kumar Gupta <sumit.gupta16@ibm.com>
2025-03-24 13:03:53 +0530

    Rename version due to changes and automation platform failures

--- plugins/module_utils/ibm_svc_utils.py
+++ plugins/module_utils/ibm_svc_utils.py
@@ -19,7 +19,7 @@ from ansible.module_utils.urls import open_url
 from ansible.module_utils.six.moves.urllib.parse import quote
 from ansible.module_utils.six.moves.urllib.error import HTTPError
 
-COLLECTION_VERSION = "2.7.0"
+COLLECTION_VERSION = "2.7.1"
 TIMEOUT = 600
 
 
